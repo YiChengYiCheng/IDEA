613d84846486d44cc40a18ed7508834a155b177d
YiCheng <Yicheng@123.com>
2022-10-31 16:01:27 +0800

    Hot_Fix Commit By IDEA

--- git_test/src/main/java/com/yicheng/GitTest.java
+++ git_test/src/main/java/com/yicheng/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("我是傻逼");
         System.out.println("我是傻逼");
+        System.out.println("在hot_fix上面");
     }
 }
